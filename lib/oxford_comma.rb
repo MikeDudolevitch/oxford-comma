85bcb57eec62e31f39158ce0f851ee41bbd729c0
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-02-02 11:48:14 -0500

    commit tree 79ef5300222451c61b4cfd7c41162fc889fac1d6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require "pry"
+ 
 def oxford_comma(array)
-
+    if array.length == 1 
+        return array.join
+elsif array.length == 2 
+        return array.join(" and ")
+else 
+    last = array.pop
+        array.join(", ") + ", and " + last
+        # if array length >= 3 join every element except the last one. 
+    end
 end
